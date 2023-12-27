880b9b04208ee76ea4f763ddd11e186faf9a2b53
Denis <den.grigorev.2002@list.ru>
2023-12-27 22:07:41 +0300

    add homework

--- Program.cs
+++ Program.cs
@@ -1,10 +1,79 @@
-﻿Console.WriteLine("введите первое число");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите первое число");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
-int squer = firstNumber * firstNumber;
-if (secondNumber == squer)
-{
-    Console.WriteLine("Yes");
-}
-else Console.WriteLine("No");
+﻿// Console.WriteLine("введите первое число");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите первое число");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (secondNumber == firstNumber * firstNumber)
+// {
+//     Console.WriteLine("Yes");
+// }
+// else Console.WriteLine("No");
+
+
+// Console.Write("введите первое число:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// if (n < 0)
+// {
+//     n = n * (-1);
+// }
+// int negative = n * (-1);
+// while (negative <= n)
+// {
+//     Console.Write(negative + " ");
+//     negative = negative + 1;
+// }
+// int firstNumber = 6;
+// int secondNumber = 6;
+// if (firstNumber < secondNumber)
+// {
+//   Console.Write("первое число " + "`" + firstNumber + "`" + " меньше чем второе число " + "`" + secondNumber + "`");
+// }
+//   if (firstNumber > secondNumber)
+//   {
+//     Console.Write("первое число " + "`" + firstNumber + "`" + " больше чем второе число " + "`" + secondNumber + "`");
+//   }
+//     if (firstNumber == secondNumber)
+//     {
+//         Console.Write("введённые числа равны " + "`" + firstNumber + "`");
+//     }
+// int a = 3;
+// int b = 8;
+// int c = 3;
+// int max = a;
+//   if (max < b)
+//   {
+//     max = b;
+//   }
+//     if (max < c)
+//     {
+//       max = c;
+//     }
+// Console.WriteLine(max);
+// int number = Convert.ToInt32(Console.ReadLine());
+// int n = 2;
+// while (n <= number)
+// {
+//   Console.Write(n + "\t");
+//   n = n + 2;
+// }
+// int number = 9;
+// if (number % 2 == 0)
+// {
+//   Console.Write("Число " + "`" + number + "`" + " чётное");
+// }
+// else Console.Write("Число " + "`" + number + "`" + " нечётное");
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// // "&&" - "И": яблоки И апельсины (и то, и другое нужно купить)
+// // "||" - "ИЛИ": яблоки ИЛИ апельсины(что-то одно, либо все вместе)
+// if (number >= 100 && number <= 999) // Число - трехзначное
+// {
+//     int firstDigit = number / 100; // 274 / 100 = 2
+//     int thirdDigit = number % 10; // 274 % 10 = 4
+//     int result = firstDigit * 10 + thirdDigit;
+//     // Интерполяция: $"Значение переменной {thirdDigit} = {}"
+//     Console.WriteLine($"Результат от числа {number} = {result}");
+// }
+// else // Число НЕ трехзначное
+// {
+//     Console.WriteLine("Число НЕ трехзначное");}
